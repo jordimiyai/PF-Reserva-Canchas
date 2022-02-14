bcda1696fc2532c7bd9279d109a105304112ad86
Eimi Suyai <jgalvan89@gmail.com>
2022-02-14 13:26:17 -0300

    deploy frontend

--- api/src/TempData/data.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE 'users' (
-    'id' INT(11) NOT NULL AUTO_INCREMENT,
-    
-)
--- api/src/routes/routerFindLocation.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const validator = require('express-joi-validation').createValidator({})
-const Joi = require('joi')
-const {findByLocation} = require('../controllers/findByLocation')
-
-router.get('/', findByLocation)
-
-module.exports = router;
